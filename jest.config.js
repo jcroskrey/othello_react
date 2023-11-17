2a4e875c98b601bb4a3b1e9dca5eecd5d1013a58
John Croskrey <jcrosk@cox.net>
2023-11-17 14:53:30 -0600

    coverage provider to 'v8'

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  coverageProvider: 'v8'
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
